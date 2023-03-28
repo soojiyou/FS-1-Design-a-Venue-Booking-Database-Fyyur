35bcbcced22fd2920f1522cadb57319054148a51
syou <dbtnwl369@gmail.com>
2023-03-28 16:36:27 -0700

    manage

--- manage.py
+++ manage.py
@@ -1,11 +1,13 @@
-from flask_script import Manager
+# from flask_script import Manager
+from flask.cli import FlaskGroup
 from flask_migrate import Migrate, MigrateCommand
 from config import app
 from flask_sqlalchemy import SQLAlchemy
 # from models import db
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-manager = Manager(app)
+# manager = Manager(app)
+manager = FlaskGroup(app)
 
 manager.add_command('db', MigrateCommand)
 
