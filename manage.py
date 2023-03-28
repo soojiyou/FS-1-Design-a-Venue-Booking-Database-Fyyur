5beecaf437230a173ee911d910c49ff2989b0b2b
syou <dbtnwl369@gmail.com>
2023-03-28 07:58:34 -0700

    heroku

--- manage.py
+++ manage.py
@@ -1,13 +1,13 @@
-# from flask_script import Manager
-from flask_migrate import Migrate
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 from config import app
 from models import db
 
 migrate = Migrate(app, db)
-# manager = Manager(app)
+manager = Manager(app)
 
-# manager.add_command('db', MigrateCommand)
+manager.add_command('db', MigrateCommand)
 
 
-# if __name__ == '__main__':
-#     manager.run()
+if __name__ == '__main__':
+    manager.run()
