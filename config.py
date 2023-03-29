3edc8e3e08d8e08a67dcc7a4e33dd776e8fe0e60
syou <dbtnwl369@gmail.com>
2023-03-28 21:26:02 -0700

    update postgres

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = SQLALCHEMY_TRACK_MODIFICATIONS
 
-SQLALCHEMY_DATABASE_URI = 'postgres://ugmqneixdnggll:68b59e32daabd81609276787159cf240f3dc80d37132e57527e972e40e3c962c@ec2-3-230-24-12.compute-1.amazonaws.com:5432/d5sljav4fnv13u'
+SQLALCHEMY_DATABASE_URI = 'postgres://sjofxezngemthe:01e4bbe6276660d0f7b0c5bb08542b8110d45cbcfc8b3d26fb28173f005a428a@ec2-52-54-200-216.compute-1.amazonaws.com:5432/d790t0rv3cfqhg'
 
 
 # SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:abc@localhost:5432/fyyur'
